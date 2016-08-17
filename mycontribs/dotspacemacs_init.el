da2de1d90f120424cfe75d34e151386bd56b1203
Totok Sulistiomono <stotok@kukisku.com>
2016-08-18 05:26:57 +0800

    [Font] Change from Andale Mono to Hack in OSX.

--- mycontribs/dotspacemacs_init.el
+++ mycontribs/dotspacemacs_init.el
@@ -17,7 +17,7 @@
   (setq-default
     ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
     ;; quickly tweak the mode-line size to make separators look not too crappy.
-    dotspacemacs-default-font '("Andale Mono"
+    dotspacemacs-default-font '("Hack"
                                 :size 12
                                 :weight normal
                                 :width normal
