c0f290915901c412cef1cab88a4da30f40311f74
Totok Sulistiomono <stotok@kukisku.com>
2016-08-23 18:39:27 +0800

    Customize ivy buffer.

--- mycontribs/dsp_abo_uc.el
+++ mycontribs/dsp_abo_uc.el
@@ -15,6 +15,11 @@
 
 (when (configuration-layer/package-usedp 'ivy)
   (message "<<< user-config: ivy customization")
+  (setq ivy-use-virtual-buffers t
+        ivy-height 12
+        ivy-count-format "(%d/%d)"
+        ivy-display-style 'fancy)
+  ;;
   ;; i don't like dir buffer. let's change :)
   (define-key ivy-minibuffer-map (kbd "RET") 'ivy-alt-done))
 
