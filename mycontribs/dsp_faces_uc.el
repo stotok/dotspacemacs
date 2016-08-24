3d413b8469c09a80729ed8325e6710d4899e4afd
Totok Sulistiomono <stotok@kukisku.com>
2016-08-24 08:16:30 +0800

    Turn on rainbow-delimiters-mode on most programming modes.

--- mycontribs/dsp_faces_uc.el
+++ mycontribs/dsp_faces_uc.el
@@ -13,4 +13,21 @@
 ;; **************************************************************************************
 ;;
 
+;;
+;; rainbow-delimiters
+;;     https://github.com/Fanael/rainbow-delimiters.git
+;;
+;; balanced and colorful delimiters
+;; https://github.com/Fanael/rainbow-delimiters
+;;
+;; Toggle single buffer: M-x rainbow-delimiters-mode
+(when (configuration-layer/package-usedp 'rainbow-delimiters) ; see if it's slow on large files
+  (message "<<< user-config: rainbow-deliminers-mode customization ...")
+  (if nil  ; only on certain mode
+      (progn
+        (add-hook 'c-mode-common-hook #'rainbow-delimiters-mode)
+        (setq-default frame-background-mode 'dark))
+    (progn ; on most programming modes (emacs 24 and above)
+      (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))))
+
 ;; EOF
