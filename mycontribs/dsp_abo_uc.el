55fd1cbfbe1a81815e61db4c01121d70c099bb36
Totok Sulistiomono <stotok@kukisku.com>
2016-08-26 04:56:21 +0800

    Recenter window on swiper exit.

--- mycontribs/dsp_abo_uc.el
+++ mycontribs/dsp_abo_uc.el
@@ -23,4 +23,21 @@
   ;; i don't like dir buffer. let's change :)
   (define-key ivy-minibuffer-map (kbd "RET") 'ivy-alt-done))
 
+(when (configuration-layer/package-usedp 'swiper)
+  (message "<<< user-config: swiper customization")
+
+  ;; simulate "*" in vim with a better swiper interface
+  (when nil                             ; let's see first
+    (define-key evil-normal-state-map (kbd "*")
+      (lambda () (interactive) (swiper (format "\\<%s\\>" (thing-at-point 'symbol)))))
+    (define-key evil-normal-state-map (kbd "#")
+      (lambda () (interactive) (swiper (format "\\<%s\\>" (thing-at-point 'word))))))
+
+  ;; advise swiper to recenter on exit
+  ;;     http://pragmaticemacs.com/emacs/dont-search-swipe/
+  (defun bjm-swiper-recenter (&rest args)
+    "recenter display after swiper"
+    (recenter))
+  (advice-add 'swiper :after #'bjm-swiper-recenter))
+
 ;; EOF
