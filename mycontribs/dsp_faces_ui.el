bcf5f20ebf9d1d8f0f99a0cc03ea2ff17a6138d9
Totok Sulistiomono <stotok@kukisku.com>
2016-08-30 05:17:59 +0800

    Make spacemacs-dark theme a bit darker.

--- mycontribs/dsp_faces_ui.el
+++ mycontribs/dsp_faces_ui.el
@@ -23,6 +23,10 @@
   (message ">>> user-init: spacemacs-theme customization ...")
   (setq spacemacs-theme-comment-bg nil) ; why the heck got background :(
   (setq spacemacs-theme-org-height nil) ; don't change height of org-mode headlines
+  ;; original color for comment is too strong
+  ;; use http://www.colorpicker.com to determine RGB value
+  (custom-set-variables '(spacemacs-theme-custom-colors
+                          '((comment . "#137C87")))) ; #2aa1ae (original spacemacs-dark)
   )
 
 ;; EOF
