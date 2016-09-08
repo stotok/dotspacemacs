be500eec4d14ee1da7d7403df5c3b998e1a9dc28
Totok Sulistiomono <stotok@kukisku.com>
2016-09-09 07:43:04 +0800

    Provide macro to determine system type and host name.

--- mycontribs/dsp_init.el
+++ mycontribs/dsp_init.el
@@ -7,13 +7,95 @@
 ;;***************************************************************************************
 
 ;; **************************************************************************************
-;; Utilities for programming mode
+;; This is called at very beginning of spacemacs initialization
 ;; **************************************************************************************
+;;
+;; Provide macro to determine operating systems and version of emacs
+;;
+;; Notes:
+;;
+;; 1. This .emacs is configured to support Emacs 24 and above only.
+;;    It is due to the usage of use-package that is available on Emacs 24 and above.
+;;
+;; 2. How to determine OS in elisp.
+;;
+;;    `system-type' is a variable defined in `C source code'.
+;;
+;;    Documentation:
+;;    Value is symbol indicating type of operating system you are using.
+;;    Special values:
+;;      `gnu'         compiled for a GNU Hurd system.
+;;      `gnu/linux'   compiled for a GNU/Linux system.
+;;      `darwin'      compiled for Darwin (GNU-Darwin, Mac OS X, ...).
+;;      `ms-dos'      compiled as an MS-DOS application.
+;;      `windows-nt'  compiled as a native W32 application.
+;;      `cygwin'      compiled using the Cygwin library.
+;;    Anything else indicates some sort of Unix system.
+;;
+;; **************************************************************************************
+;;
+;; Determine the OS
+;; On Linux
+(defmacro IsGNULinux ()
+  (eq system-type 'gnu/linux))
+;; On Windows --> Native emacs for windows
+(defmacro IsWindows ()
+  (eq system-type 'windows-nt))
+;; On Cygwin --> Cygwin emacs-w32
+(defmacro IsCygwin ()
+  (eq system-type 'cygwin))
+;; On Mac OS X
+(defmacro IsOSX ()
+  (eq system-type 'darwin))
+;;
+;; Determine hostname
+;;
+(defmacro IsBraeburn ()
+  (string= system-name "braeburn"))
+(defmacro IsSG2-SATT93 ()
+  (string= system-name "SG2-SATT93.msg.asia.wdc.com"))
+(defmacro IsKKB-SATT93 ()
+  (string= system-name "kkb-satt93"))
+(defmacro IsCrux ()
+  (string= system-name "crux"))
+(defmacro IsPolaris ()
+  (or (string= system-name "polaris") (string= system-name "polaris.local") (string= system-name "polaris.hgst.com")))
+;;
+;; Display OS and HOSTNAME information
+;;
+(when t
+  ;; DISPLAY OS
+  (cond
+   ((IsWindows)
+    (message "IsWindows: TRUE"))
+   ((IsCygwin)
+    (message "IsCygwin: TRUE"))
+   ((IsGNULinux)
+    (message "IsGNULinux: TRUE"))
+   ((IsOSX)
+    (message "IsOSX: TRUE"))
+   (t
+    (message "Unknown OS.")))
+  ;; DISPLAY HOSTNAME
+  (cond
+   ((IsBraeburn)
+    (message "IsBraeburn: TRUE"))
+   ((IsSG2-SATT93)
+    (message "IsSG2-SATT93: TRUE"))
+   ((IsKKB-SATT93)
+    (message "IsKKB-SATT93: TRUE"))
+   ((IsCrux)
+    (message "IsCrux: TRUE"))
+   ((IsPolaris)
+    (message "IsPolaris: TRUE"))
+   (t
+    (message "Unknown HOSTNAME."))))
+;;
 ;;
 ;; Fonts
 ;;
 (cond
- ((eq system-type 'darwin)
+ ((IsOSX)
   (setq-default
     ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
     ;; quickly tweak the mode-line size to make separators look not too crappy.
@@ -36,7 +118,7 @@
                           anti-zenburn
                           )
     )) ;; darwin
- ((eq system-type 'gnu/linux)
+ ((IsGNULinux)
   (setq-default
     ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
     ;; quickly tweak the mode-line size to make separators look not too crappy.
@@ -59,7 +141,7 @@
                           anti-zenburn
                           )
     )) ;; gnu/linux
- ((or (eq system-type 'cygwin) (eq system-type 'windows-nt))
+ ((or (IsCygwin) (IsWindows))
   ;; This is to fix error when calling server start on startup
   (require 'server)
   (and (>= emacs-major-version 23)
