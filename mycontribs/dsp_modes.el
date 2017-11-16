5f86e4425b7ccfdd5a69afb78ce0b7a9598916c9
Totok Sulistiomono <stotok@kukisku.com>
2017-11-16 21:59:20 +0800

    Add c mode style 2.

--- mycontribs/dsp_modes.el
+++ mycontribs/dsp_modes.el
@@ -38,6 +38,27 @@
       (c-echo-syntactic-information-p . t))
     "My C Programming Style")
   ;;
+  (defconst my-c-style-2
+    '((c-tab-always-indent        . t)
+      (c-comment-only-line-offset . 0) ;; 4
+      (c-hanging-braces-alist     . ((substatement-open after)
+                                    (brace-list-open)))
+      (c-hanging-colons-alist     . ((member-init-intro before)
+                                    (inher-intro)
+                                    (case-label after)
+                                    (label after)
+                                    (access-label after)))
+      (c-cleanup-list             . (scope-operator
+                                    empty-defun-braces
+                                    defun-close-semi))
+      (c-offsets-alist            . ((arglist-close . c-lineup-arglist)
+                                    (substatement-open . 0)
+                                    (case-label        . 3) ;; 0 ;; 4
+                                    (block-open        . 0)
+                                    (knr-argdecl-intro . -)))
+      (c-echo-syntactic-information-p . t))
+    "My C Programming Style 2")
+  ;;
   (defconst om-c-style
     '((c-basic-offset                    . 4)
       ;; anyway, below is the default
@@ -90,6 +111,8 @@
   ;(setq c-offsets-alist '((member-init-intro . ++)))
   ;;
   ;; Customizations for all of modes in CC mode
+  ;;
+  ;; my-c-mode hook
   (defun my-c-mode-common-hook ()
     ;; add my personal style and set it for the current buffer
     (c-add-style "PERSONAL" my-c-style t)
@@ -102,11 +125,36 @@
           indent-tabs-mode nil ; nil uses space, t uses tab
           )
     ;; we like auto-newline and hungry-delete
-  ;  (c-toggle-auto-hungry-state 1)
+    ;; (c-toggle-auto-hungry-state 1)
     ;; keybindings for all supported languages.  We can put these in
     ;; c-mode-base-map because c-mode-map, c++-mode-map, objc-mode-map,
     ;; java-mode-map, and idl-mode-map inherit from it.
-    (define-key c-mode-base-map "\C-m" 'newline-and-indent))
+    (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+    )
+  ;; my-c-mode-2 hook
+  (defun my-c-mode-2-common-hook ()
+    ;; add my personal style and set it for the current buffer
+    (c-add-style "PERSONAL2" my-c-style-2 t)
+    ;; offset customizations not in my-c-style
+    (c-set-offset 'member-init-intro '++)
+    ;; treat '_' as a word constituent
+    (modify-syntax-entry ?_ "w")
+    ;; local binding
+    (local-set-key (kbd "C-,") 'c-up-conditional-with-else)
+    (local-set-key (kbd "C-.") '(lambda (count) (interactive "p") (c-up-conditional-with-else (- count))))
+    ;; other customizations
+    (setq tab-width 4
+          c-basic-offset 4
+          c-indent-level 4
+          indent-tabs-mode nil ; nil uses space, t uses tab
+          )
+    ;; we like auto-newline and hungry-delete
+    ;; (c-toggle-auto-hungry-state 1)
+    ;; keybindings for all supported languages.  We can put these in
+    ;; c-mode-base-map because c-mode-map, c++-mode-map, objc-mode-map,
+    ;; java-mode-map, and idl-mode-map inherit from it.
+    (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+    )
   ;; om hook
   (defun om-c-mode-common-hook ()
     (c-add-style "OM" om-c-style t)
@@ -114,7 +162,8 @@
     (modify-syntax-entry ?_ "w")
     ;; local binding
     (local-set-key (kbd "C-,") 'c-up-conditional-with-else)
-    (local-set-key (kbd "C-.") '(lambda (count) (interactive "p") (c-up-conditional-with-else (- count)))))
+    (local-set-key (kbd "C-.") '(lambda (count) (interactive "p") (c-up-conditional-with-else (- count))))
+    )
   ;; sb hook
   (defun sb-c-mode-common-hook ()
     (c-add-style "SB" sb-c-style t)
@@ -122,10 +171,11 @@
     ;;(modify-syntax-entry ?_ "w")
     ;; local binding
     (local-set-key (kbd "C-,") 'c-up-conditional-with-else)
-    (local-set-key (kbd "C-.") '(lambda (count) (interactive "p") (c-up-conditional-with-else (- count)))))
+    (local-set-key (kbd "C-.") '(lambda (count) (interactive "p") (c-up-conditional-with-else (- count))))
+    )
   ;;
   ;; apply which one to use
-  (add-hook 'c-mode-common-hook 'sb-c-mode-common-hook)
+  (add-hook 'c-mode-common-hook 'my-c-mode-2-common-hook)
   ;; files *.ipp is c++ source code in UPA
   (setq auto-mode-alist (cons '("\\.ipp$" . c++-mode) auto-mode-alist))
   )
