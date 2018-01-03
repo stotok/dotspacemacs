51abe9f58812426c4b5df7278e3c4fbf2010767b
Totok Sulistiomono <stotok@sentinel.singnet.com.sg>
2018-01-03 21:31:13 +0800

    org-drill leech item treated as normal item.

--- mycontribs/dsp_org.el
+++ mycontribs/dsp_org.el
@@ -160,6 +160,7 @@
       (setq org-drill-hide-item-headings-p nil)      ; show heading during drill
       (setq org-drill-add-random-noise-to-intervals-p t)
       (setq org-drill-learn-fraction 0.2)    ; to appear more frequently (default: 0.5)
+      (setq org-drill-leech-method nil)      ; leech treated same as normal items
       ;;
       ) ;; with-eval-after-load 'org
     ;;
