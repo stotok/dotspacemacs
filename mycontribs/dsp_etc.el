0eb27c90985e374bcad71ba0f90dbd2ded284a33
Totok Sulistiomono <stotok@kukisku.com>
2018-02-24 02:06:42 +0800

    Using digits to select company-mode candidates.

--- mycontribs/dsp_etc.el
+++ mycontribs/dsp_etc.el
@@ -82,7 +82,41 @@
     (message "*** dsp_etc.el user config: company customization")
     ;; this will enable company-mode in all buffers
     (message "*** dsp_etc.el user config: global company mode enabled")
-    (global-company-mode t))
+    (global-company-mode t)
+    ;;
+    ;; using digits to select company-mode candidates
+    ;; https://oremacs.com/2017/12/27/company-numbers/
+    ;; basic setting
+    (setq company-show-numbers t)       ; numbers show next to candidate
+    ;; add some bindings
+    (let ((map company-active-map))
+      (mapc (lambda (x) (define-key map (format "%d" x) 'ora-company-number))
+       (number-sequence 0 9))
+      (define-key map " " (lambda ()
+                            (interactive)
+                            (company-abort)
+                            (self-insert-command 1)))
+      (define-key map (kbd "<return>") nil))
+    ;; actual code
+    (defun ora-company-number ()
+      "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+      (interactive)
+      (let* ((k (this-command-keys))
+             (re (concat "^" company-prefix k)))
+        (if (cl-find-if (lambda (s) (string-match re s))
+                        company-candidates)
+            (self-insert-command 1)
+          (company-complete-number
+           (if (equal k "0")
+               10
+             (string-to-number k))))))
+    ;;
+    ;; end of using digits to select company-mode candidates
+    ;;
+    )
   )
  )
 
