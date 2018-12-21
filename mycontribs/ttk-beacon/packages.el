d0ca3aadb15b651caace1766520fd015a85a6113
Totok Sulistiomono <stotok@kukisku.com>
2018-12-22 05:06:11 +0800

    Enable package beacon

--- mycontribs/ttk-beacon/packages.el
+++ mycontribs/ttk-beacon/packages.el
@@ -1,13 +1,13 @@
 ;; -*- coding:utf-8-unix mode:emacs-lisp -*-
 ;;***************************************************************************************
 ;;
-;; 2016 Totok Sulistiomono
+;; 2016-2018 Totok Sulistiomono
 ;; stotok@kukisku.com
 ;;
 ;;***************************************************************************************
 (setq ttk-beacon-packages
       '(
-        beacon 
+        beacon
         ))
 
 (defun ttk-beacon/init-beacon ()
@@ -17,9 +17,13 @@
   ;; never lose your cursor again
   (use-package beacon
     :init
-    (setq beacon-push-mark 0)             ; dunno what, just set it to 0 :)
+    (setq beacon-push-mark 0)           ; won't push the mark
     (setq beacon-color "#666600")
+    (setq beacon-blink-when-point-moves-vertically 10) ; min lines vertical movement
+    (setq beacon-blink-when-window-scrolls nil) ; no need when scrolls
     (setq beacon-blink-when-focused nil)  ; blink when emacs gains focus, don't want :)
+    (setq beacon-blink-duration 0.5)      ; seconds that the blink should last
+    (setq beacon-blink-delay 0.3)         ; seconds before starting to fade the beacon
     :config
     (beacon-mode t)))
 
