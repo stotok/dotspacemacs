ea17df97a5d308111d464085b4c50dc09619a308
Totok Sulistiomono <stotok@kukisku.com>
2019-03-05 06:18:31 +0800

    Added keybinding for switch buffer to ivy-occur etags

--- mycontribs/ttk-counsel-etags/keybindings.el
+++ mycontribs/ttk-counsel-etags/keybindings.el
@@ -8,14 +8,23 @@
 ;;
 ;; see counsel-etags
 ;;
+(defun ivy-occur-display-counsel-egrep ()
+  "Display the *ivy-occur* buffer."
+  (interactive)
+  (let ((buffer (get-buffer "*ivy-occur counsel-etags-find-tag \"\"*")))
+    (if buffer
+        (pop-to-buffer buffer)
+      (error"The *ivy-occur cousel-etags-find-tag \"\"* buffer does not exisst yet"))))
+
 (dolist (mode '(c-mode c++-mode))
   (spacemacs/declare-prefix-for-mode mode "me" "etags")
   (spacemacs/set-leader-keys-for-major-mode mode
-    "eb"   'counsel-etags-recent-tag
+    "eb"   'ivy-occur-display-counsel-egrep
     "ed"   'counsel-etags-find-tag-at-point
     "es"   'counsel-etags-grep-symbol-at-point
     "eS"   'counsel-etags-find-tag
     "eu"   'xref-pop-marker-stack
+    "er"   'counsel-etags-recent-tag
     )
 )
 
